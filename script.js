e0b4de183c8a69c4a36aa26cda67f1c96e48f356
Gogulan12 <gogulanravi12@gmail.com>
2022-10-30 16:02:16 -0400

    added requirements to contact form

--- script.js
+++ script.js
@@ -17,45 +17,25 @@ document.querySelectorAll(".main-menu li a").forEach((n) =>
 
 // // ADDED FORM SUBMITION ON CONTACT AND BLOG
 
-// // Find the form element on the page
-// const formElement = document.querySelector("form");
-
-// // Add an event listener for the 'submit' event
-// formElement.addEventListener("submit", function () {
-//   // When the form is submitted...
-//   // get the task from the input
-//   // add the task as a list-item to the <ul>
-// });
-
 const formElement = document.querySelector(".message-box button");
 
 const formName = document.querySelector(".message-box input[name=name]");
-console.log(formName);
+// console.log(formName);
 const formEmail = document.querySelector(".message-box input[name=email");
-console.log(formEmail);
+// console.log(formEmail);
+const formText = document.querySelector(".message-box textarea[name=message]");
+// console.log(formText);
 
-// formElement.addEventListener("click", function () {
-//   if (formName.value.length == 0 || formEmail.value.length == 0) {
-//     console.log("Please fill all fields!");
-//     window.alert("please fill all fields!");
-//   } else {
-//     console.log("submitted!!");
-//     window.alert("Message submitted!");
-//     formName.value = "";
-//     formEmail.value = "";
-//     console.log("cleared");
-//   }
-// });
-
-// formElement.addEventListener("click", function () {
-//   if (formEmail.value.length == 0 ) {
-//     console.log("please fill field");
-//   } else {
-//     console.log("submit");
-//   }
-// });
-
-// formElement.addEventListener("click", function () {
-//   formName.value = "";
-//   console.log("cleared");
-// });
+formElement.addEventListener("click", function () {
+  if (
+    formName.value.length == 0 ||
+    formEmail.value.length == 0 ||
+    formText.value.length == 0
+  ) {
+    // console.log("Please fill all fields!");
+    window.alert("please fill all fields!");
+  } else {
+    // console.log("submitted!!");
+    window.alert("Message submitted!");
+  }
+});
